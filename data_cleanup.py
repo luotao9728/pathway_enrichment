7c101809f76b69a9d0c804f68ba5c2ffe41d82b2
luotao9728 <112722913+luotao9728@users.noreply.github.com>
2023-03-23 21:01:59 -0400

    Small changes and create a Jupiter file for testing

--- data_cleanup.py
+++ data_cleanup.py
@@ -4,7 +4,9 @@ import pandas as pd
 
 def read_file(filepath):
     df = pd.read_csv(filepath, sep='\t', skiprows=[0,2,3,4,5])
-    df = df.drop(index=[0,1,2,3])
+    components = filepath.split('/')
+    df['sample_type'] = components[1]
+    df['tumor_name'] = components[0]
     return df
 
 df = read_file('KICH/CANCER/gdc_download_20230301_212055.202248/0ba21ef5-0829-422e-a674-d3817498c333/4868e8fc-e045-475a-a81d-ef43eabb7066.rna_seq.augmented_star_gene_counts.tsv')
